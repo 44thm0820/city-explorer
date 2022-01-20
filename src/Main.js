07e348aa893ef4f3d8cb733e97e3c95ed202ff15
Rey Mercado <reymercado.usa@gmail.com>
2022-01-19 19:59:02 -0800

    wip initial-commit before deploying to netlify

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <main>
+        <h3>Proof of life in main</h3>
+      </main>
+    );
+  }
+}
+
+export default Main;
